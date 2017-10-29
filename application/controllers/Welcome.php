3c6803d8cd86710b02bae9641a2d613ac2f05dad
unknown <pablo.esteban@zte.com.cn>
2017-10-28 19:48:34 -0500

    creacion de todos los daos y unso cambios en bd

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,13 +6,10 @@ class Welcome extends CI_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model('data/dao_user_model');
     }
 
     public function index() {
-      $dao = new dao_user_model();
-      $res = $dao->getCustom4($this->request);
-      $this->json($res);
+      echo "hola gente";
     }
 
 }
