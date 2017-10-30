484c2eb38dfd644c0b901f9474819bc716c5f785
bredybuitrago <bredybuitrago@gmail.com>
2017-10-30 15:18:21 -0500

    se agregan nuevos daos

--- application/models/data/dao_precheck_model.php
+++ application/models/data/dao_precheck_model.php
@@ -7,6 +7,36 @@
     class dao_precheck_model extends CI_Model{
 
         public function __construct(){
+          $this->load->model('dto/PrecheckModel');
         }
+
+        public function getAll(){
+          try {
+            $precheck = new PrecheckModel();
+            $datos = $precheck->get();
+            $response = new Response(EMessages::SUCCESS);
+            $response->setData($datos);
+            return $response;
+          } catch (ZolidException $ex) {
+            return $ex;
+          }
+        }
+
+        public function getUserByprecheck($idUser){
+          try {
+            $precheck = new PrecheckModel();
+            $datos = $precheck->where("k_id_user","=",$idUser)
+                          ->first();
+            $response = new Response(EMessages::SUCCESS);
+            $response->setData($datos);
+            return $response;
+
+          } catch (ZolidException $ex) {
+            return $ex;
+          }
+
+
+        }
+
     }
 ?>
