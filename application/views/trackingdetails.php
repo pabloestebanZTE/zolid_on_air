d76d8b5d889db736bbe2f2292977289a262c0e06
Jarvanux <john.vanegas67@gmail.com>
2017-11-01 11:13:00 -0500

    PULL

--- application/views/trackingdetails.php
+++ application/views/trackingdetails.php
@@ -419,22 +419,10 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-                                    Collapsible Group 3</a>
-                            </h4>
-                        </div>
-                        <div id="collapse3" class="panel-collapse collapse">
-                            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                commodo consequat.</div>
-                        </div>
-                    </div>
-                </div>                        
+                    </div>                    
+                </div>
+                <div class="">
+                </div>
             </div>                
         </form>
         <!-- FIN TRACKING DETAILS FORM -->
--- assets/js/modules/tracking-details.js
+++ assets/js/modules/tracking-details.js
@@ -5,17 +5,6 @@ var TD = {
     },
     events: function () {
         $('#btnDetails').on('click', TD.onClickDetails);
-        $('.stepwizard-step a').on('click', TD.stepClick);
-    },
-    stepClick: function (e) {
-        app.stopEvent(e);
-        var step = $(this);
-        var content = step.parents('.stepwizard').parent();
-        var stepPanel = content.find('.step-panel' + step.attr('href'));
-        content.find('.stepwizard-step a').attr('class', 'btn btn-default btn-circle');
-        step.attr('class', 'btn btn-primary btn-circle');
-        content.find('.step-panel').addClass('hidden');
-        stepPanel.removeClass('hidden').hide().fadeIn(500);
     },
     configView: function () {
         dom.configCalendar($('#txtFechaIngresoOnAir'));
